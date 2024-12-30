93af6a91d05ba261bc8b31931926564fc9dbe848
Kandd13 <nguyenduykhang0703@gmail.com>
2024-12-30 18:56:37 +0700

    Update jest.system.config.js

--- healthy_empowerer/jest.system.config.js
+++ healthy_empowerer/jest.system.config.js
@@ -2,4 +2,14 @@ module.exports = {
   preset: "jest-expo",
   testMatch: ["**/__tests__/system/**/*.test.[jt]s?(x)"],
   verbose: true,
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test-results",
+        outputName: "junit.xml",
+      },
+    ],
+  ],
 };
