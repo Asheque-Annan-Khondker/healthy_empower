647b22eff203dbc7e5b133ebd3d6013b48ded858
Syed Anas Rehan <sar983@uowmail.edu.au>
2025-01-24 21:15:44 +1100

    Changes

--- healthy_empowerer/components/__tests__/__integration__/sampleIntegration.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { render } from '@testing-library/react-native';
-
-test('renders a simple component', () => {
-  const { getByText } = render(<Text>Hello World</Text>);
-  expect(getByText('Hello World')).toBeTruthy();
-});
