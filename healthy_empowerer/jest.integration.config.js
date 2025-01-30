247b9e51d46366fc2d1f1544fb32d99cb6c85c68
Syed Anas Rehan <sar983@uowmail.edu.au>
2025-01-30 19:50:56 +1100

    More unit tests

---
+++ healthy_empowerer/jest.integration.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //preset: 'jest-expo',
+  preset: 'react-native',
+  testMatch: ['**/__integration__/**/*.test.[jt]s?(x)'], // Look for tests in the tests/integration folder
+  testEnvironment: 'node',
+  verbose: true,
+    
+};
