130f71f7136e3eb1e5d0d5098c56d1a6e65c596f
lujke-dunn <lukedunn011@gmail.com>
2025-01-31 15:55:15 +1100

    added user post request and let user-service listen on port 3000

---
+++ user-service/src/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors'); 
+
+
+const app = express(); 
+const port = 3000; 
+
+app.use(express.json());
+app.use(cors());
+
+// we use a map until we implement sqlLite and ORM
+const users = new Map();
+const healthProfiles = new Map(); 
+
+// TODO 1/2/25 write endpoint test, hash_password, ensure post validation, handle email validation, and handle duplicates
+
+
+// create a user with non-health information 
+app.post(
+  '/api/users', (req, res) => {
+    try {
+      // password is currently unhashed
+      const { username, email, password_hash, date_of_birth, gender, timezone } = req.body; 
+      
+      
+      const newUser = { 
+         id: Date.now().toString(); 
+         username, 
+         email, 
+         password_hash,
+         date_of_birth,
+         gender, 
+         timezone, 
+         created_at: new Date(), 
+         last_login: new Date()
+       }; 
+
+
+      users.set(userId, newUser); 
+      // omit password_hash from response
+      const { password_hash: _, ...userResponse} = newUser; 
+
+      // send the data back 
+      res.status(201).json(userResponse);
+      } catch (error) {
+        res.status(500).json({error: error.message });
+        }
+      }
+);
+
+app.listen(port, () => {
+  console.log(`user-service listening on port ${port}`)
+})
+
