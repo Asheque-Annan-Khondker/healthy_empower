c486db07b7cdc8dc8b7661e22868573977bcf2b7
Syed Anas Rehan <sar983@uowmail.edu.au>
2025-02-17 20:31:57 +1100

    if nested elements render correctly

--- healthy_empowerer/components/__tests__/ThemedText-test.tsx
+++ healthy_empowerer/components/__tests__/ThemedText-test.tsx
@@ -13,3 +13,12 @@ it('renders correctly with styling props', () => {
     const tree = renderer.create(<ThemedText style={{ fontSize: 20, color: 'blue' }}>Styled text</ThemedText>).toJSON();
     expect(tree).toMatchSnapshot();
   });
+ it('renders correctly with nested elements', () => {
+    const tree = renderer.create(
+      <ThemedText>
+        <span>Nested Element</span>
+      </ThemedText>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  }); 
+  
