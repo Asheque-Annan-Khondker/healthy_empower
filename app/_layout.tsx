0d59eb99728c7505c708ef78ced3694f86088e56
Safal Sapkota <49337062+Safal30@users.noreply.github.com>
2025-04-05 22:04:19 +1100

    Updated PaperProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,16 +6,20 @@ import {Drawer} from "expo-router/drawer";
 import {createNativeStackNavigator} from "react-native-screens/native-stack";
 import {createDrawerNavigator} from "@react-navigation/drawer";
 import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+
 
 export default function Layout() {
   return (
+    <PaperProvider>
 
-    <GestureHandlerRootView>
-      <Stack screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-          <Stack.Screen name="(drawer)" options={{ headerShown:false}} />
-      </Stack>
-    </GestureHandlerRootView>
+      <GestureHandlerRootView>
+        <Stack screenOptions={{headerShown: false}}>
+          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+            <Stack.Screen name="(drawer)" options={{ headerShown:false}} />
+        </Stack>
+      </GestureHandlerRootView>
+    </PaperProvider>
   );
 }
 // const StackNav = createNativeStackNavigator()
