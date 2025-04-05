fea28a88bc7d007424fa095dab85f6ab9a053abc
sar983 <sar983@uowmail.edu.au>
2025-04-05 20:47:45 +1100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  preset: 'jest-expo', // or 'ts-jest' if TypeScript
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(sequelize|pg|other-esm-packages)/)',
-  ],
-};
+
