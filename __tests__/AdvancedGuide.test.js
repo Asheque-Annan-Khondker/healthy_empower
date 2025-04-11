2a8e2ac67c7f10cbfd2e2eb4e0ed4154ccad99f9
sar983 <sar983@uowmail.edu.au>
2025-04-11 19:18:44 +1000

    Deleting AdvancedGuide.test.js

--- __tests__/AdvancedGuide.test.js
+++ __tests__/AdvancedGuide.test.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import AdvancedGuide from '../app/(drawer)/(guide)/AdvancedGuide';
 
-
-jest.mock('@/components/screenTransition', () => {
-  return ({ children }: any) => <>{children}</>; 
-});
-
-describe('AdvancedGuide', () => {
-  it('renders the correct guide text', () => {
-    const { getByText } = render(<AdvancedGuide />);
-    expect(getByText('Guide for advanced')).toBeTruthy();
-  });
-});
