5332afe68097d4fcd9b8a93045204a83bca95f79
lujke-dunn <lukedunn011@gmail.com>
2025-05-17 15:07:17 +1000

    style changes made

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -8,48 +8,21 @@ import {FAIcon, MatIcon} from "@/utils/getIcon";
 import {View} from "react-native";
 import {Appbar} from "react-native-paper";
 import {DrawerToggleButton} from "@react-navigation/drawer";
-import {useNavigation} from "expo-router";
+import {Tabs, useNavigation} from "expo-router";
 import {DrawerActions} from "@react-navigation/native";
-export default function tablayout() {
-  const tabs = createMaterialBottomTabNavigator()
-    const navigation = useNavigation()
+
+export default function TabLayout() {
+  // Super simple version to test if it works
   return (
-      // * having flex, the entire screen isnt consumed by the appbar
-
-      <View style={{flex: 1}}>
-
-          <Appbar.Header>
-              <Appbar.Action icon={"arrow-left"} onPress={()=>{navigation.dispatch(DrawerActions.toggleDrawer())}} />
-          </Appbar.Header>
-    <tabs.Navigator style={{flex: 1}}>
-
-      <tabs.Screen
-        name="diet_screen"
-        component={DietScreen}
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen 
+        name="index" 
         options={{
-          title: 'diet',
-          tabBarIcon: ({ color }) => <FAIcon size={28} name="square" color={color} />,
+          title: 'Home',
+          tabBarLabel: 'Home'
         }}
       />
-
-        <tabs.Screen
-            name="index"
-            component={Index}
-            options={{
-                title: 'home',
-                tabBarIcon: ({ color }) => <FAIcon size={28} name="home" color={color} />,
-            }}
-        />
-        <tabs.Screen
-            name={"achievement_screen"}
-            component={AchievementScreen}
-            options={{
-                title: 'achievements',
-                tabBarIcon: ({ color }) => <FAIcon size={28} name="trophy" color={color} />,
-            }}
-        />
-    </tabs.Navigator>
-      </View>
+    </Tabs>
   );
 }
 
