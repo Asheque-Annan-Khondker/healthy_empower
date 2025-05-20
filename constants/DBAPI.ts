a9718eb3e374e6e98716dbf0ef3c857f3fa70ce5
jd547 <jonathan.duong13@gmail.com>
2025-05-21 03:25:31 +1000

    jono laptop access

--- constants/DBAPI.ts
+++ constants/DBAPI.ts
@@ -3,5 +3,8 @@
 // Android emulator special IP for localhost
 //export const API_URL = 'http://10.0.2.2:3001';
 
-// jono's IP
-export const API_URL = 'http://192.168.1.104:3001';
+// jono's IP laptop
+export const API_URL = 'http://192.168.1.134:3001';
+
+// jono's IP PC
+//export const API_URL = 'http://192.168.1.104:3001';
